555c2568c69ef8996d90954d014476b862e942f5
Josiah Olajide <josiaholajide@gmail.com>
2022-05-31 17:26:26 +0100

    Recreate sound board project

--- 09-sound-board/index.js
+++ 09-sound-board/index.js
@@ -3,19 +3,19 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 sounds.forEach(sound => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
+    btn.innerText = sound;
 
     btn.addEventListener('click', () => {
         stopSongs()
         document.getElementById(sound).play()
     })
 
-    btn.innerText = sound
     document.getElementById('buttons').appendChild(btn)
 })
 
 function stopSongs() {
     sounds.forEach(sound => {
-        const song = document.getElementById(sound)
+        song = document.getElementById(sound);
 
         song.pause();
         song.currentTime = 0;
