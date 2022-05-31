ae559c2831f14f479b35c2fa93605fa5779c8109
Josiah Olajide <josiaholajide@gmail.com>
2022-05-31 19:13:58 +0100

    Final commit for dad jokes project

---
+++ 10-Dad-Jokes/index.js
@@ -0,0 +1,16 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('joke-btn');
+
+generateJoke()
+
+jokeBtn.addEventListener('click', generateJoke)
+
+async function generateJoke() {
+    const config = {
+        headers: {'Accept': 'application/json'}
+    }
+    
+    const response = await fetch('https://icanhazdadjoke.com/', config)
+    const data =  await response.json()
+    jokeEl.innerText = data.joke
+}
