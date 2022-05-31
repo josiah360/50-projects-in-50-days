fc0807d207b514f3542b8be67caf467f08ec90a7
Josiah Olajide <josiaholajide@gmail.com>
2022-05-31 16:02:02 +0100

    Recreate Form Wave

--- 08-form-wave-animation/index.js
+++ 08-form-wave-animation/index.js
@@ -1,8 +1,8 @@
-const labels = document.querySelectorAll('.form-control label');
+const labels = document.querySelectorAll('.form-controller label');
 
 labels.forEach(label => {
     label.innerHTML = label.innerText
-    .split('')
-    .map((letter, idx) => `<span style="transition-delay: ${idx * 20}ms">${letter}</span>`)
-    .join('')
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 30}ms">${letter}</span>`)
+        .join('')
 })
