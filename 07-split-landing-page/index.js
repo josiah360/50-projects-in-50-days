018a9a75e1f0b797345d7c634067f7a2dfadf2fa
Josiah Olajide <josiaholajide@gmail.com>
2022-05-31 11:57:33 +0100

    Final commit on split landing page project

--- 07-split-landing-page/index.js
+++ 07-split-landing-page/index.js
@@ -1,21 +1,10 @@
-const bg = document.querySelector('.bg');
-const loadingText = document.querySelector('.loading-text');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
 
-let load = 0;
+left.addEventListener('mouseover', () => container.classList.add('hover-left'));
+left.addEventListener('mouseout', () => container.classList.remove('hover-left'));
 
-const int = setInterval(blurring, 30)
 
-function blurring() {
-    load += 1;
-
-    if(load > 99) {
-        clearInterval(int)
-    }
-    loadingText.innerText = `${load}%`;
-    loadingText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-}
-
-function scale (number, inMin, inMax, outMin, outMax) {
-    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-}
+right.addEventListener('mouseover', () => container.classList.add('hover-right'));
+right.addEventListener('mouseout', () => container.classList.remove('hover-right'));
