9438ab28f6255db526ec2e9868fd1b45bbce74da
Josiah Olajide <josiaholajide@gmail.com>
2022-06-01 21:59:06 +0100

    Html done

--- 14-animation-navigation/index.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector('.container');
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', () => {
-    container.classList.toggle('active')
-})
