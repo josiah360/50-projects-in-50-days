9bf4eeb86bbc7b8665957da4ded01a4f001a0376
Josiah Olajide <josiaholajide@gmail.com>
2022-06-07 19:47:59 +0100

    To be continued

--- 05-blurry-loading/index.js
+++ 05-blurry-loading/index.js
@@ -1,21 +1,17 @@
+const counter = document.querySelector('.text');
 const bg = document.querySelector('.bg');
-const loadingText = document.querySelector('.loading-text');
 
-let load = 0;
+let count = 0;
 
-const int = setInterval(blurring, 30)
+const interval = setInterval(update, 40)
 
-function blurring() {
-    load += 1;
+function update() {
+    count += 1;
+    counter.innerText = count;
+    counter.style.opacity = `${(count/100) * 1}`
 
-    if(load > 99) {
-        clearInterval(int)
+    if(count === 100) {
+        clearInterval(interval)
     }
-    loadingText.innerText = `${load}%`;
-    loadingText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-function scale (number, inMin, inMax, outMin, outMax) {
-    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-}
