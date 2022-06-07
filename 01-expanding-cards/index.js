0ad87d2349f426f6a9cf65569110e09472451cca
Josiah Olajide <josiaholajide@gmail.com>
2022-06-07 16:28:26 +0100

    Redesign expanding cards

--- 01-expanding-cards/index.js
+++ 01-expanding-cards/index.js
@@ -2,13 +2,10 @@ const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClass()
+        panels.forEach(panel => {
+            panel.classList.remove('active')
+        })
+
         panel.classList.add('active')
     })
 })
-
-function removeActiveClass() {
-    panels.forEach(panel => {
-        panel.classList.remove('active');
-    })
-}
