b68a7d320c522aba0fb4bcbcfba3e70c1bbb56f8
Josiah Olajide <josiaholajide@gmail.com>
2022-06-07 17:39:21 +0100

    Redesigned progress steps

--- 02-progress-steps/index.js
+++ 02-progress-steps/index.js
@@ -1,69 +1,65 @@
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 const circles = document.querySelectorAll('.circle');
-const progress = document.querySelector('.progress')
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
+const range = document.querySelector('.range');
 
-let currentActive = 1;
+let current = 1;
 
-next.addEventListener('click', () => {
-    currentActive += 1;
-    updateCurrentActive()
+nextBtn.addEventListener('click', () => {
+    current += 1;
+
+    updateCurrent()
     updateCircles()
+    updateRange()
     updateBtn()
-    progressWidth()
-
+    
 })
 
-prev.addEventListener('click', () => {
-    currentActive -= 1;
-    updateCurrentActive()
-    updateCircles()
-    updateBtn()
-    progressWidth()
+prevBtn.addEventListener('click', () => {
+    current -= 1;
 
+    updateCurrent()
+    updateCircles()
+    updateRange()
+    updateBtn()
 })
 
-function progressWidth() {
-    const activeCircles = document.querySelectorAll('.circle.active');
-    const progressWidth = ((activeCircles.length - 1)/(circles.length - 1)) * 100;
-    progress.style.width = `${progressWidth}%`
-    console.log(progressWidth)
-}
-
-function updateCurrentActive() {
-    if(currentActive > circles.length) {
-        currentActive = circles.length;
+function updateBtn() {
+    if(current > 1) {
+        prevBtn.disabled = false;
+    } else {
+        prevBtn.disabled = true;
     }
 
-    if(currentActive < 1) {
-        currentActive = 1
+    if(current < circles.length) {
+        nextBtn.disabled = false;
+    } else {
+        nextBtn.disabled = true;
+    }
+}
+
+function updateCurrent() {
+    if(current > circles.length) {
+        current = circles.length;
+    } 
+
+    if(current < 0) {
+        current = 0;
     }
 }
 
 function updateCircles() {
     circles.forEach((circle, idx) => {
-        if(currentActive > idx) {
-            circle.classList.add('active')
-        } else {
+        if(idx < current) {
+            circle.classList.add('active');
+        } else{
             circle.classList.remove('active')
         }
     })
 }
 
-function updateBtn() {
-    if(currentActive > 1) {
-        prev.disabled = false;
-    }
-
-    if(currentActive === circles.length) {
-        next.disabled = true
-    }
-
-    if (currentActive === 1) {
-        prev.disabled = true
-    }
-
-    if(currentActive < circles.length) {
-        next.disabled = false
-    }
+function updateRange() {
+    const activeCircles = document.querySelectorAll('.circle.active').length;
+    const totalCircles = circles.length;
+    range.style.width = `${((activeCircles - 1)/(totalCircles - 1)) * 100}%`
 }
