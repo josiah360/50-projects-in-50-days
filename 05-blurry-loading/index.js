40f3de55692b91fd1b382006edcff381d677270a
Josiah Olajide <josiaholajide@gmail.com>
2022-06-08 17:19:31 +0100

    Redesigned blurry loading project

--- 05-blurry-loading/index.js
+++ 05-blurry-loading/index.js
@@ -7,11 +7,15 @@ const interval = setInterval(update, 40)
 
 function update() {
     count += 1;
-    counter.innerText = count;
-    counter.style.opacity = `${(count/100) * 1}`
+    counter.innerText = `${count}%`;
+    counter.style.opacity = `${scale(count, 0, 100, 1, 0)}`
+    bg.style.filter = `blur(${scale(count, 0, 100, 35, 0)}px)`
 
     if(count === 100) {
         clearInterval(interval)
     }
 }
 
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
