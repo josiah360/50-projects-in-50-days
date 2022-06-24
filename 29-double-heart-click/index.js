734660805815ff97b988122f2a65a5eb76631017
Josiah Olajide <josiaholajide@gmail.com>
2022-06-24 20:22:45 +0100

    Final commit for double heart

--- 29-double-heart-click/index.js
+++ 29-double-heart-click/index.js
@@ -1,3 +1,42 @@
 const loveMe = document.querySelector('.love-me');
+const times = document.getElementById('times');
 
-console.log(loveMe)
+let clickTime = 0;
+let timesClicked = 0
+
+
+loveMe.addEventListener('dblclick', (e) => {
+    findXandY(e)
+})
+
+function findXandY(e) {
+    const elementTop = loveMe.offsetTop;
+    const elementLeft = loveMe.offsetLeft;
+
+    const cX = e.clientX
+    const cY = e.clientY
+
+    const x = cX - elementLeft
+    const y = cY - elementTop
+    
+    createHeart(x, y)
+    incrementLike()
+}
+
+function createHeart(x, y) {
+    const heartElement = document.createElement('i')
+    heartElement.classList.add('fas')
+    heartElement.classList.add('fa-heart')
+    heartElement.classList.add('heart')
+
+    heartElement.style.left = `${x}px`
+    heartElement.style.top = `${y}px`
+
+    loveMe.appendChild(heartElement)
+
+    window.setTimeout(() => heartElement.remove(), 600)
+}
+
+function incrementLike() {
+    times.innerText = ++ timesClicked
+}
