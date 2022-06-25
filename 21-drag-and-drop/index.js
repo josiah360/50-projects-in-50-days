95ae379062c822cfa91a0c98cc11e2ddec478fb6
Josiah Olajide <josiaholajide@gmail.com>
2022-06-25 16:31:04 +0100

    final commit

--- 21-drag-and-drop/index.js
+++ 21-drag-and-drop/index.js
@@ -2,9 +2,11 @@ const boxes = document.querySelectorAll('.box');
 const frame = document.querySelector('.fill');
 
 
+frame.addEventListener('dragstart', dragStart)
+frame.addEventListener('dragend', dragEnd)
+
+
 boxes.forEach(box => {
-    box.addEventListener('dragstart', dragStart)
-    box.addEventListener('dragend', dragEnd)
     box.addEventListener('dragover', dragOver)
     box.addEventListener('dragenter', dragEnter)
     box.addEventListener('dragleave', dragLeave)
@@ -13,25 +15,28 @@ boxes.forEach(box => {
 
 
 function dragStart() {
-
+    frame.className += ' hold'
+    setTimeout(() => frame.className = 'invisible', 0)
 }
 
 function dragEnd() {
-    
+    frame.className = 'fill'
 }
 
-function dragOver() {
-    
+function dragOver(e) {
+    e.preventDefault()
 }
 
-function dragEnter() {
-    
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
 }
 
 function dragLeave() {
-    
+    this.className = 'box'
 }
 
 function dragDrop() {
-    
+    this.appendChild(frame)
+    this.className = 'box'
 }
