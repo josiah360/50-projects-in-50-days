4bc32729c62e441e972aefa0d9dbe1111f4859cc
Josiah Olajide <josiaholajide@gmail.com>
2022-06-27 17:06:08 +0100

    Final commit

--- 26-double-vertical-slider/index.js
+++ 26-double-vertical-slider/index.js
@@ -1,1 +1,33 @@
 const leftSlider = document.querySelector('.left-slider');
+const rightSlider = document.querySelector('.right-slider');
+const btnUp = document.querySelector('.btn-up');
+const btnDown = document.querySelector('.btn-down');
+const slidesLength = document.querySelectorAll('.left-slider .slide').length
+
+leftSlider.style.top = `-${100 * (slidesLength - 1)}%`;
+let activeIndex = 0;
+
+btnDown.addEventListener('click', () => changeSlide('down'));
+btnUp.addEventListener('click', () => changeSlide('up'));
+
+function changeSlide(direction) {
+    if(direction === 'down') {
+        ++activeIndex
+        
+        if(activeIndex > slidesLength - 1) {
+            activeIndex = 0;
+        }
+    }
+
+    if(direction === 'up') {
+        --activeIndex
+        
+        if(activeIndex < 0) {
+            activeIndex = slidesLength - 1;
+        }
+    }
+
+    leftSlider.style.transform = `translateY(${100 * activeIndex}%)`
+    rightSlider.style.transform = `translateY(-${100 * activeIndex}%)`
+}
+
