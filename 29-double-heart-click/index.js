41456a4e7f773929d0e8407a4343366bd3048a41
Josiah Olajide <josiaholajide@gmail.com>
2022-06-29 01:47:27 +0100

    Final

--- 29-double-heart-click/index.js
+++ 29-double-heart-click/index.js
@@ -1,14 +1,30 @@
 const img = document.querySelector('.img')
+const count = document.querySelector('.count')
 
-img.addEventListener('click', (e) => {
+let like = 0
+
+function putShape(e) {
     const x = e.clientX - img.offsetLeft
     const y = e.clientY - img.offsetTop
-    
+
     createHeart(x, y, img)
-})
+    incrementLike(count)
+}
+
+img.addEventListener('click', putShape)
 
 function createHeart(x, y, elem) {
     const heart = document.createElement('i')
-    heart.classList.add('fas', 'fa-heart')
+    heart.classList.add('fas', 'fa-heart', 'grow-heart')
+    heart.style.left = `${x}px`
+    heart.style.top = `${y}px`
     elem.appendChild(heart)
+
+    setTimeout(() => heart.remove(), 600)
 }
+
+function incrementLike(likeElem) {
+    like += 1
+    likeElem.innerText = like
+}
+
