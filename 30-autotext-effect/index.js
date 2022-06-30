769999fd4c65fca115aca89a5a51af4622425bc3
Josiah Olajide <josiaholajide@gmail.com>
2022-06-30 11:50:26 +0100

    final commit for autotext

--- 30-autotext-effect/index.js
+++ 30-autotext-effect/index.js
@@ -1,8 +1,37 @@
-const text = document.querySelector('h1')
+const h1 = document.querySelector('h1')
 const speed = document.querySelector('#speed')
 
+
 let typeSpeed = 1;
+const text = 'We Love Programming!';
+let textLength = 0;
+
+let typeForward = true;
+
+speed.addEventListener('input', () => {
+    typeSpeed = +speed.value
+})
 
 function updateText() {
-    const text = text.innerText
+    h1.innerText = text.slice(0, textLength)
+
+    if(typeForward) {
+        textLength += 1; 
+    } else {
+        textLength -= 1
+    }
+    
+
+    if(textLength > text.length) {
+        typeForward = false;
+    }
+
+    if(textLength < 1) {
+        textLength = 1
+        typeForward = true;
+    }
+   
+    setTimeout(updateText, 300 / typeSpeed) 
 }
+
+updateText()
